7310a09acb0c585a59df1a3cf23edbfdacca5fc8
DimaMukhin <dima9348@gmail.com>
2019-07-07 16:22:32 -0500

    implemented user

--- src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.rest.webservices.restfulwebservices;
+package com.example.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

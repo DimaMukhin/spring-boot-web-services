a85df519a777d3df897a6ec61958f48ce5889694
DimaMukhin <dima9348@gmail.com>
2019-07-07 16:13:06 -0500

    implemented hello world object endpoint

--- src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class HelloWorldController {
 	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	public String helloWorld() {
 		return "hello world";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
